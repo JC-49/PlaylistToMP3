4b0bbec1bd87d66579931270e65eec1772d62560
JC-49 <103142275+JC-49@users.noreply.github.com>
2024-02-23 21:31:05 +0100

    Add title and author file tagging

--- download.py
+++ download.py
@@ -1,5 +1,49 @@
+import os
+import eyed3
+import re
+import tempfile
 from pytube import YouTube
+from moviepy.editor import AudioFileClip
+import shutil
 
-def download_from_url(url, output_path):
+def sanitize_filename(filename):
+    return re.sub(r'[<>:"/\\|?*]', '', filename)
+
+def download_from_url(url):
+    output_path = os.getcwd() + "/output"
+    temp_path = tempfile.mkdtemp()
     yt = YouTube(url)
-    yt.streams.filter(only_audio=True).first().download(output_path)
+    print(f"Downloading: {url} - {yt.title}")
+    title_parts = yt.title.split(' - ')
+    if len(title_parts) > 1 and title_parts[1].strip().upper() not in ['HARDSTYLE', 'FRENCHCORE', ':RE']:
+        artist = yt.author  + '; ' +  title_parts[0]
+        title = ' - '.join(title_parts[1:])
+    else:
+        artist = yt.author
+        title = yt.title
+
+    sanitized_title = sanitize_filename(yt.title)
+    mp3_file = output_path + "/" + sanitized_title + ".mp3"
+
+    # Check if the file already exists
+    if os.path.exists(mp3_file):
+        print(f"File {mp3_file} already exists. Skipping download.")
+        return
+
+    video_stream = yt.streams.filter(only_audio=True).first()
+    video_file = video_stream.download(temp_path)  
+    audio_clip = AudioFileClip(video_file)
+   
+    audio_clip.write_audiofile(mp3_file)
+
+    # Delete the temporary directory
+    shutil.rmtree(temp_path)
+
+    # Edit MP3 metadata
+    audiofile = eyed3.load(mp3_file)
+    if (audiofile.tag == None):
+        audiofile.initTag()
+
+    audiofile.tag.artist = artist
+    audiofile.tag.title = title
+    audiofile.tag.save()
--- start.py
+++ start.py
@@ -2,20 +2,15 @@ from dotenv import load_dotenv
 import os, urls, download
 import concurrent.futures
 
-def download_url(url):
-    print(f"Downloading {url}")
-    output_path = os.getcwd() + "/output"
-    download.download_from_url(url, output_path)
-
 if __name__ == "__main__":
-    n = 5
+    n = int(input("Enter the number of videos to download: "))
     load_dotenv()
     playlist_url = os.getenv("PLAYLIST_URL")
     last_n_urls = urls.get_last_n_video_urls(playlist_url, n)
     print(f"Last {n} Video URLs:")
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(download_url, last_n_urls)
+        executor.map(download.download_from_url, last_n_urls)
 
-    print("Download complete.")
+    print("Download and Conversion complete.")
     #input("Press Enter to exit...")
